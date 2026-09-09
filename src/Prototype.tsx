c1d1cc6c5c23026dfe040393e94aec5681220e0b
Richard Mutakwa <mutakwarichard11@gmail.com>
2026-09-09 17:28:01 +0200

    Update proposal passcode

--- src/Prototype.tsx
+++ src/Prototype.tsx
@@ -17,7 +17,7 @@ import { MobileScroll } from "./mobile";
 import "./prototype.css";
 
 const CONTENT = {
-  passcode: "0824",
+  passcode: "0826",
   herName: "Beautiful",
   yourName: "Richard",
   letter: [
